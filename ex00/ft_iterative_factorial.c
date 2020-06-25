2d43d5e2e6fe62b55fb23d72bce7027e0b51fd63
Mbuso21 <newmbuso@gmail.com>
2020-06-25 11:00:51 +0200

    ex00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmehloma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 07:59:56 by mmehloma          #+#    #+#             */
+/*   Updated: 2020/06/25 10:59:03 by mmehloma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.c>
+
+int ft_iterative_factorial(int nb)
+{
+	int	ft_iterative_factorial(int nb)
+	{
+		int				i;
+		long long int	r;
+
+		if (nb < 0)
+		{
+			return (0);
+		}
+		i = 2;
+		r = 1;
+		while (i <= nb)
+		{
+			r *= i;
+			++i;
+		}
+		return (r);
+	}
+
+
